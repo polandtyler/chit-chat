4986186e2d0a91be1158f2a22a7a3705d8943a1a
Tyler Poland <poland.tyler@gmail.com>
2019-09-02 11:29:22 -0500

    chapter 2-3

--- route_main.go
+++ route_main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/polandtyler/chit_chat/data"
-	"html/template"
 	"net/http"
 )
 
@@ -18,15 +17,24 @@ func err(writer http.ResponseWriter, request *http.Request) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html",
-	}
-
-	templates := template.Must(template.ParseFiles(files...))
+	//files := []string{
+	//	"templates/layout.html",
+	//	"templates/navbar.html",
+	//	"templates/index.html",
+	//}
+	//
+	//templates := template.Must(template.ParseFiles(files...))
+	//threads, err := data.Threads()
+	//if err == nil {
+	//	templates.ExecuteTemplate(w, "layout", threads)
+	//}
 	threads, err := data.Threads()
 	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
+		_, err := session(w, r)
+		if err != nil {
+			generateHTML(w, threads, "layout", "public_navbar", "index")
+		} else {
+			generateHTML(w, threads, "layout", "private_navbar", "index")
+		}
 	}
 }
